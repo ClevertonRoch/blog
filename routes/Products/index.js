57db694c1390e291f180f80ced1ceb4cfe303a44
ClevertonRoch <websistematizando@gmail.com>
2023-06-05 21:03:07 -0300

    feat: implemant product rota file

--- routes/Products/index.js
+++ routes/Products/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const productController = require('./../../controllers/ProductsControllers')
+const subCatSchema = require('../../middleware/validationYup/schemasYup/productSchema')
+const validaDataYup = require('../../middleware/validationYup/validationData')
 
 
-
-// router.get('/products', productController.create)
-router.post('/product', productController.create)
+router.get('/subcategory', productController.findAll)
+router.get('/subcategory/:id', validaDataYup(subCatSchema.id, 'params'), productController.findByPk)
+router.post('/subcategory', validaDataYup(subCatSchema.create, 'body'), productController.create)
+router.put('/subcategory', validaDataYup(subCatSchema.update, 'body'), productController.update)
+router.delete('/subcategory/:id?', validaDataYup(subCatSchema.id, 'params'), productController.destroy)
 
 
 
